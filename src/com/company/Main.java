9a54fb0f6aee48ae90d15dd34c86b781626f3c34
Lucian Pop <lucian.pop@itiviti.com>
2021-12-06 13:02:29 +0200

    Initial commit
    Change-Id: I24c58ce7730245bcb65c930c19a2edbbd402a41f

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int myFirstInt = 3;
+        System.out.println(myFirstInt);
+
+    }
+}
